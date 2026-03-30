9ce8ffe0e9f686863e9861e8444a6c8169fd67d2
SebastianQuis <sebastianglg64@gmail.com>
2026-03-30 00:03:47 -0500

    creando nuevo modulo para dragonball z

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-page-component',
+  templateUrl: './main-page.component.html',
+})
+
+export class MainComponent {
+
+}
